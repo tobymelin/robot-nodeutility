6dc2bf638c9b737feeb75081649666a189249fa0
Toby <tm@ao>
2019-09-24 17:19:11 +0100

    Initial commit, v1.2

---
+++ RobotMovementTool/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace RobotMovementTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonMove = new System.Windows.Forms.RadioButton();
+            this.radioButtonCopy = new System.Windows.Forms.RadioButton();
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.coordInput = new System.Windows.Forms.TextBox();
+            this.comboBoxCoords = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioRelativeNode = new System.Windows.Forms.RadioButton();
+            this.radioRelative = new System.Windows.Forms.RadioButton();
+            this.radioAbsolute = new System.Windows.Forms.RadioButton();
+            this.buttonGetCoords = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButtonMove
+            // 
+            this.radioButtonMove.AutoSize = true;
+            this.radioButtonMove.Checked = true;
+            this.radioButtonMove.Location = new System.Drawing.Point(12, 19);
+            this.radioButtonMove.Name = "radioButtonMove";
+            this.radioButtonMove.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonMove.TabIndex = 0;
+            this.radioButtonMove.TabStop = true;
+            this.radioButtonMove.Text = "Move";
+            this.radioButtonMove.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCopy
+            // 
+            this.radioButtonCopy.AutoSize = true;
+            this.radioButtonCopy.Location = new System.Drawing.Point(12, 42);
+            this.radioButtonCopy.Name = "radioButtonCopy";
+            this.radioButtonCopy.Size = new System.Drawing.Size(49, 17);
+            this.radioButtonCopy.TabIndex = 1;
+            this.radioButtonCopy.Text = "Copy";
+            this.radioButtonCopy.UseVisualStyleBackColor = true;
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Location = new System.Drawing.Point(118, 40);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(75, 23);
+            this.buttonExecute.TabIndex = 2;
+            this.buttonExecute.Text = "Execute";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            this.buttonExecute.Click += new System.EventHandler(this.ButtonExecute_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClose.Location = new System.Drawing.Point(118, 69);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // coordInput
+            // 
+            this.coordInput.Location = new System.Drawing.Point(13, 13);
+            this.coordInput.Name = "coordInput";
+            this.coordInput.Size = new System.Drawing.Size(134, 20);
+            this.coordInput.TabIndex = 0;
+            // 
+            // comboBoxCoords
+            // 
+            this.comboBoxCoords.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCoords.FormattingEnabled = true;
+            this.comboBoxCoords.Items.AddRange(new object[] {
+            "XYZ",
+            "X",
+            "Y",
+            "Z"});
+            this.comboBoxCoords.Location = new System.Drawing.Point(155, 13);
+            this.comboBoxCoords.Name = "comboBoxCoords";
+            this.comboBoxCoords.Size = new System.Drawing.Size(49, 21);
+            this.comboBoxCoords.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonMove);
+            this.groupBox1.Controls.Add(this.radioButtonCopy);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(80, 71);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Edit Mode";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioRelativeNode);
+            this.groupBox2.Controls.Add(this.radioRelative);
+            this.groupBox2.Controls.Add(this.radioAbsolute);
+            this.groupBox2.Location = new System.Drawing.Point(12, 127);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(193, 92);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Coordinate Options";
+            // 
+            // radioRelativeNode
+            // 
+            this.radioRelativeNode.AutoSize = true;
+            this.radioRelativeNode.Location = new System.Drawing.Point(11, 66);
+            this.radioRelativeNode.Name = "radioRelativeNode";
+            this.radioRelativeNode.Size = new System.Drawing.Size(105, 17);
+            this.radioRelativeNode.TabIndex = 7;
+            this.radioRelativeNode.Text = "Relative to Node";
+            this.radioRelativeNode.UseVisualStyleBackColor = true;
+            // 
+            // radioRelative
+            // 
+            this.radioRelative.AutoSize = true;
+            this.radioRelative.Location = new System.Drawing.Point(11, 42);
+            this.radioRelative.Name = "radioRelative";
+            this.radioRelative.Size = new System.Drawing.Size(123, 17);
+            this.radioRelative.TabIndex = 6;
+            this.radioRelative.Text = "Relative Coordinates";
+            this.radioRelative.UseVisualStyleBackColor = true;
+            // 
+            // radioAbsolute
+            // 
+            this.radioAbsolute.AutoSize = true;
+            this.radioAbsolute.Checked = true;
+            this.radioAbsolute.Location = new System.Drawing.Point(11, 19);
+            this.radioAbsolute.Name = "radioAbsolute";
+            this.radioAbsolute.Size = new System.Drawing.Size(125, 17);
+            this.radioAbsolute.TabIndex = 5;
+            this.radioAbsolute.TabStop = true;
+            this.radioAbsolute.Text = "Absolute Coordinates";
+            this.radioAbsolute.UseVisualStyleBackColor = true;
+            // 
+            // buttonGetCoords
+            // 
+            this.buttonGetCoords.Location = new System.Drawing.Point(118, 98);
+            this.buttonGetCoords.Name = "buttonGetCoords";
+            this.buttonGetCoords.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetCoords.TabIndex = 9;
+            this.buttonGetCoords.Text = "Get Coords.";
+            this.buttonGetCoords.UseVisualStyleBackColor = true;
+            this.buttonGetCoords.Click += new System.EventHandler(this.ButtonGetCoords_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonExecute;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonClose;
+            this.ClientSize = new System.Drawing.Size(218, 231);
+            this.Controls.Add(this.buttonGetCoords);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBoxCoords);
+            this.Controls.Add(this.coordInput);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonExecute);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Move/Copy";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonMove;
+        private System.Windows.Forms.RadioButton radioButtonCopy;
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TextBox coordInput;
+        private System.Windows.Forms.ComboBox comboBoxCoords;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioRelativeNode;
+        private System.Windows.Forms.RadioButton radioRelative;
+        private System.Windows.Forms.RadioButton radioAbsolute;
+        private System.Windows.Forms.Button buttonGetCoords;
+    }
+}
+
---
+++ RobotMovementTool/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RobotOM;
+
+namespace RobotMovementTool
+{
+    public partial class Form1 : Form
+    {
+        RobotApplication iapp;
+        RobotStructure structure;
+        RobotNodeServer nodes;
+        RobotBarServer bars;
+        RobotSelection selection;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text += " v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            comboBoxCoords.SelectedIndex = 0;
+
+        }
+
+        private void ButtonExecute_Click(object sender, EventArgs e)
+        {
+            iapp = new RobotApplication();
+
+            if (iapp.Project.IsActive == 0)
+                return;
+
+            Vctr movementVector = new Vctr();
+            var str_coord = "";
+
+            structure = iapp.Project.Structure;
+            nodes = structure.Nodes;
+            bars = structure.Bars;
+            selection = structure.Selections.Get(IRobotObjectType.I_OT_NODE);
+
+            if (radioRelativeNode.Checked)
+            {
+                try
+                {
+                    str_coord = ReturnCoordString(ValidateNodeID(coordInput.Text), comboBoxCoords.Text);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    ErrorDialog("Invalid node number input", "ERROR: Node Not Found");
+                    return;
+                }
+            }
+            else
+                str_coord = coordInput.Text.Replace(" ", "");
+
+            for (int i = 1; i <= selection.Count; i++)
+            {
+                var node = (IRobotNode)nodes.Get(selection.Get(i));
+
+                movementVector.X = node.X;
+                movementVector.Y = node.Y;
+                movementVector.Z = node.Z;
+
+                if (comboBoxCoords.SelectedItem.ToString() == "XYZ")
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        movementVector.Move(j.ToString(), ValidateSingleCoord(str_coord, j), radioRelative.Checked);
+                    }
+                }
+                else
+                    movementVector.Move(comboBoxCoords.SelectedItem.ToString(), ValidateSingleCoord(str_coord), radioRelative.Checked);
+
+                if (radioButtonMove.Checked)
+                {
+                    node.X = movementVector.X;
+                    node.Y = movementVector.Y;
+                    node.Z = movementVector.Z;
+                }
+                else
+                    nodes.Create(nodes.FreeNumber, movementVector.X, movementVector.Y, movementVector.Z);
+            }
+        }
+
+        private double ValidateSingleCoord(string str_coord, int idx = 0)
+        {
+            double output;
+            Regex chr = new Regex(@"[a-zA-Z]");
+
+            if (chr.Match(str_coord).Success)
+                throw new ArgumentOutOfRangeException();
+
+            str_coord = str_coord.Replace(" ", "");
+
+            string[] str_firstpart = str_coord.Split(',');
+
+            if (!Double.TryParse(str_firstpart[idx], out output))
+                throw new ArgumentOutOfRangeException();
+
+            return output;
+        }
+
+        private int ValidateNodeID(string input)
+        {
+            Regex chr = new Regex(@"[a-zA-Z]");
+
+            if (chr.Match(input).Success)
+                throw new ArgumentOutOfRangeException();
+
+            string[] str_split = input.Split(',');
+            return int.Parse(str_split[0]);
+        }
+
+        private string ReturnCoordString(int node_id, string axis = "XYZ")
+        {
+            IRobotNode curr_node;
+            try
+            {
+                curr_node = (IRobotNode)nodes.Get(node_id);
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            string output_str = "";
+
+            if (axis == "X")
+                output_str = curr_node.X.ToString();
+            else if (axis == "Y")
+                output_str = curr_node.Y.ToString();
+            else if (axis == "Z")
+                output_str = curr_node.Z.ToString();
+            else
+            {
+                output_str += curr_node.X.ToString();
+                output_str += "," + curr_node.Y.ToString();
+                output_str += "," + curr_node.Z.ToString();
+            }
+
+            return output_str;
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ErrorDialog(string text, string caption)
+        {
+            MessageBox.Show(text, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void ButtonGetCoords_Click(object sender, EventArgs e)
+        {
+            iapp = new RobotApplication();
+
+            if (iapp.Project.IsActive == 0)
+                return;
+
+            structure = iapp.Project.Structure;
+            nodes = structure.Nodes;
+            bars = structure.Bars;
+            selection = structure.Selections.Get(IRobotObjectType.I_OT_NODE);
+
+
+            int nodeNumber;
+
+            if (radioRelativeNode.Checked)
+                nodeNumber = ValidateNodeID(coordInput.Text);
+            else
+                nodeNumber = selection.Get(1);
+
+            IRobotNode node;
+
+
+            try
+            {
+                node = (IRobotNode)nodes.Get(nodeNumber);
+            }
+            catch (System.Runtime.InteropServices.COMException)
+            {
+                ErrorDialog("Invalid node number input", "ERROR: Node Not Found");
+                return;
+            }
+
+            string coordString = node.X.ToString() + ", " + node.Y.ToString() + ", " + node.Z.ToString();
+
+            MessageBox.Show(coordString);
+        }
+    }
+
+    public class Vctr
+    {
+        public double X, Y, Z;
+
+        public Vctr()
+        {
+            X = Y = Z = 0.0;
+        }
+
+        public Vctr(double X, double Y, double Z)
+        {
+            X = this.X; Y = this.Y; Z = this.Z;
+        }
+
+        public void Move(string axis, double dist, bool relative = true)
+        {
+            if (relative)
+            {
+                if (axis == "X" || axis == "0")
+                    X += dist;
+                else if (axis == "Y" || axis == "1")
+                    Y += dist;
+                else if (axis == "Z" || axis == "2")
+                    Z += dist;
+            }
+            else {
+                if (axis == "X" || axis == "0")
+                    X = dist;
+                else if (axis == "Y" || axis == "1")
+                    Y = dist;
+                else if (axis == "Z" || axis == "2")
+                    Z = dist;
+            }
+        }
+    }
+}
